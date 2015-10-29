21a9e382e8d4a56d7054353f6f6bee92eb8b527d
banseok <seok6711@naver.com>
2015-10-29 23:53:56 +0900

    first Commit

---
+++ test/joinResult.php
@@ -0,0 +1,141 @@
+<?php include 'template/header.php'; ?>
+<div class="jumbotron">
+  <h1> 회원가입 완료</h1>
+  <p>회원가입을 축하합니다.</p>
+  <p>
+    <!-- <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs »</a> -->
+  </p>
+</div>
+
+<?php include 'template/navigation.php';
+    $userid = $_POST["userid"];
+    $password = $_POST["password"];
+    $passwordCheck = $_POST["passwordCheck"];
+    $fullName = $_POST["fullName"];
+    $email = $_POST["email"];
+    $birth = $_POST["birth"];
+    $phone = $_POST["phone"];
+
+    // Database connect
+    $servername = "localhost";
+    $username = "root";
+    $password = "1111";
+    $dbname = "appledb";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password,$dbname);
+
+    // Check connection
+    if ($conn->connect_error){
+          die("Connection failed: ".$conn->connect_error);
+    }
+    echo "<h3>Connected successfully</h3>";
+
+    $sql = "insert into users(userid,password,fullname,birth,email,phone,reg_date)
+    values('".$userid."','".$password."','".$fullName."',".$birth.",'".$email."',".$phone.","."now())";
+
+      if($conn->query($sql) == TRUE){
+       echo "<h3>insert user successfully</h3>";
+     }else{
+       echo "<h3>Error insert user</h3>".$conn->error;
+     }
+
+
+?>
+<article>
+  <div class="col-sm-9">
+    <div class="row">
+      <div class="col-sm-3">
+        <a class="navbar-brand" href="#">아이디</a>
+      </div>
+      <div class="col-sm-6">
+        <div class="form-group">
+            <?php
+              echo $userid;
+            ?>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3">
+        <a class="navbar-brand" href="#">password</a>
+      </div>
+      <div class="col-sm-6">
+        <div class="form-group">
+          <?php
+            echo $password;
+          ?>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3">
+        <a class="navbar-brand" href="#">password check</a>
+      </div>
+      <div class="col-sm-6">
+        <div class="form-group">
+          <?php
+            echo $passwordCheck;
+          ?>
+        </div>
+      </div>
+    </div>
+  <div class="row">
+    <div class="col-sm-3">
+      <a class="navbar-brand" href="#">userName</a>
+    </div>
+    <div class="col-sm-6">
+      <div class="form-group">
+        <?php
+          echo $fullName;
+        ?>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-3">
+      <a class="navbar-brand" href="#">phone</a>
+    </div>
+    <div class="col-sm-6">
+      <div class="form-group">
+        <?php
+          echo $phone;
+        ?>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-3">
+      <a class="navbar-brand" href="#">birth</a>
+    </div>
+    <div class="col-sm-6">
+      <div class="form-group">
+        <?php
+          echo $birth;
+        ?>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-3">
+      <a class="navbar-brand" href="#">email</a>
+    </div>
+    <div class="col-sm-6">
+      <div class="form-group">
+        <?php
+          echo $email;
+        ?>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-3">
+    </div>
+    <div class="col-sm-3">
+      <a href="http://192.168.0.29:8080/test/loginForm.php" >Login</button>
+    </div>
+  </div>
+</form>
+  </div>
+</article>
+<?php include 'template/footer.php'; ?>
---
+++ test/reservationForm.php
@@ -0,0 +1,15 @@
+<?php include 'template/header.php'; ?>
+<div class="jumbotron">
+  <h1> Reservation</h1>
+  <p>this page is about Reservation</p>
+  <p>
+    <!-- <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs »</a> -->
+  </p>
+</div>
+
+<?php include 'template/navigation.php'; ?>
+<article>
+  <div class="col-xs-6 col-sm-6">Reservation</div>
+  <div class="col-xs-6 col-sm-6">나의 예약 상태는 000입니다.</div>
+</article>
+<?php include 'template/footer.php'; ?>
