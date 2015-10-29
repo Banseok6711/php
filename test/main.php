4f44121a3f28e3e62362e786bc08f996376f1048
banseok <seok6711@naver.com>
2015-10-30 00:06:03 +0900

    Revert "Revert "first Commit""
    This reverts commit 80f62c260d014c44b543fa36af26eddb5a3056e2.

---
+++ test/main.php
@@ -0,0 +1,35 @@
+<?php include "template/header.php" ?>
+      <div class="jumbotron">
+        <h1>Apple A/S Center </h1>
+        <p class="lead">안녕하십니다. 애플As 센터 해운대 센텀시티점입니다. 저희 센터는 고객들을 편의를 위해서
+          온라인 예약 시스템을 운영중입니다. 방문하시기 전에 미리 예약을 하시면 기다리는 시간을 최소한으로 줄여서
+          업무를 처리 받으실 수 있습니다. 많은 이용바랍니다. 감사합니다.
+        </p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">예약 하기</a></p>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <h2>About</h2>
+          <p>저희 해운대 센텀 Apple A/S 서비스점은 Am 10:00 - Pm 7:00 까지 서비스를 하고 있으며
+          필요시에는 야간근무를 하면서 고객들에게 최상의 서비스를 제공하고자 합니다.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Contact</h2>
+          <p>연락처:051-111-2222<br/>
+             Fax: 051-111-2333<br/>
+            주소: 해운대구 센텀시티 23로 애플빌딩 204호
+            대표자: 이반석
+          </p>
+          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+       </div>
+        <div class="col-md-4">
+          <h2>Reservation</h2>
+          <p>현재 예약가능한 시간은 000 입니다.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+        </div>
+      </div>
+  </div>
+  </body>
+</html>
